8735ff4ff9bd109afd0db5e93c77dcbdb9ac67d9
chaoweixiao <chaoweixiao@0587337536.wireless.umich.net>
2016-04-19 03:42:42 -0400

    fix bug

--- edmonds.py
+++ edmonds.py
@@ -94,7 +94,7 @@ class Matching:
 	        for v in range(0, nvertex):
 	            if self.mate[v] == -1 and nodeList[v].label == 0:
 	                self.assignLabel(v, 1, -1)
-
+	        print self.mate
 	        augmented = 0
 	        while 1:
 
@@ -367,7 +367,7 @@ class Matching:
 	                    bestedgeto[bj] = k
 	        self.nodeList[bv].blossombestedges = None
 	        self.nodeList[bv].bestedge = -1
-	    self.nodeList[b_id].blossombestedges = [k for k in bestedgeto if k != 1]
+	    self.nodeList[b_id].blossombestedges = [k for k in bestedgeto if k != -1]
 	    self.nodeList[b_id].bestedge = -1
 	    for k in self.nodeList[b_id].blossombestedges:
 	        if self.nodeList[b_id].bestedge == -1 or self.slack(k) < self.slack(self.nodeList[b_id].bestedge):
@@ -505,7 +505,10 @@ class Matching:
 
 # Unit tests
 # if __name__ == '__main__':
-
+match = Matching()
+match.initEdges([(0, 3, 1070), (0, 4, 1877), (1, 3, 2246), (1, 4, 2002), (3, 4, 3415)])
+print match.maxWeightmatching()
+# print" [ -1, 8, 3, 2, 7, 6, 5, 4, 1 ]"
 	# import unittest, math
 
 	# class MaxWeightMatchingTests(unittest.TestCase):
